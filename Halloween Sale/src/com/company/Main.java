ee03d66850dee8869ce52ab857bc096984a57b40
Joniyed Bhuiyan <jbjoniyed7@gmail.com>
2019-11-28 22:15:04 +0600

    28/11/19

---
+++ Halloween Sale/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int p = input.nextInt();
+        int d = input.nextInt();
+        int m = input.nextInt();
+        int s = input.nextInt();
+        int res = 0;
+        int count=p;
+
+        while(count<s){
+            res++;
+            p = Math.max(m,p-d);
+            count += p;
+        }
+        System.out.println(res);
+    }
+}
