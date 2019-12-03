d2283561c9907d8c24d363fdbb520e789ea14982
Joniyed Bhuiyan <jbjoniyed7@gmail.com>
2019-12-03 20:06:10 +0600

    2/12/2019

--- Making_Anagrams/src/com/company/Main.java
+++ Making_Anagrams/src/com/company/Main.java
@@ -9,29 +9,32 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String str = input.next();
         String str1 = input.next();
-        List<Character> temp = new ArrayList<>();
-        for(int i=0;i<str.length();i++){
-            temp.add(str.charAt(i));
-        }
-        List<Character> temp1 = new ArrayList<>();
-        for(int i=0;i<str1.length();i++){
-            temp1.add(str1.charAt(i));
-        }
-        Collections.sort(temp);
-        Collections.sort(temp1);
+        String str2 = input.next();
 
-//        int res=0;
-//        System.out.println(temp+"\n"+temp1);
-//        for(int i=0;i<temp.size();i++){
-//            if(temp.get(i)!=temp1.get(i)){
-//                res++;
-//                temp.remove(i);
-//                temp1.remove(i);
-//                i--;
-//            }
-//        }
-        System.out.println(temp+"\n"+temp1);
+        List<Integer> str1Freq = new ArrayList<>();
+        List<Integer> str2Freq = new ArrayList<>();
+
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
+
+        for(int i=0;i<26;i++){
+            str1Freq.add(0);
+            str2Freq.add(0);
+        }
+        for(int i=0;i<str1.length();i++){
+            str1Freq.set(str1.charAt(i)-65,str1Freq.get(str1.charAt(i)-65)+1);
+        }
+
+        for(int i=0;i<str2.length();i++){
+            str2Freq.set(str2.charAt(i)-65,str2Freq.get(str2.charAt(i)-65)+1);
+        }
+
+        int res = 0;
+
+        for(int i=0;i<26;i++){
+            res+=Math.abs(str1Freq.get(i)-str2Freq.get(i));
+        }
+        System.out.println(res);
     }
 }
