9e30b95487d0a30556d156504df2864e09e966fe
Joniyed Bhuiyan <jbjoniyed7@gmail.com>
2020-02-07 21:10:30 +0600

    7 feb 2020

--- SearchFromList/src/com/company/Main.java
+++ SearchFromList/src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("Enter a String: ");
         String userInput = input.next();
         long start = System.currentTimeMillis();
-        File file = new File("C:\\Users\\joniyed\\Desktop\\routine.txt");
+        File file = new File("D:\\Intelijj_Idea\\SearchFromList\\data.txt");
         StringBuilder stringBuilder = new StringBuilder();
 
         try {
@@ -26,36 +26,37 @@ public class Main {
         }
         String str = stringBuilder.toString();
         SortedSet<String> l1 = new TreeSet<>(Arrays.asList(str.split(" ")));
-        List<String> l2 = new ArrayList<>(Arrays.asList(str.split(" ")));
+        int total_wrod = (new ArrayList<String>(Arrays.asList(str.split(" "))).size());
 
         List<String> list = new ArrayList<>(l1);
-        List<String> temp = new ArrayList<>();
         List<String> temp1 = new ArrayList<>();
 
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).charAt(0) == userInput.charAt(0) && list.get(i).length()>=userInput.length()) {
-                    temp1.add(list.get(i));
+        list.forEach( value ->
+        {
+            if(value.charAt(0)==userInput.charAt(0) && value.length()>=userInput.length()){
+                temp1.add(value);
+            }
+        });
+
+        list.clear();
+        list.addAll(temp1);
+
+        for(int i=1;i<userInput.length();i++){
+            for(int j=0;j<list.size();j++){
+                if(!(list.get(j).charAt(i)==userInput.charAt(i))){
+                    list.remove(j);
+                    j--;
                 }
             }
+        }
 
-            for(int i=1;i<userInput.length();i++){
-                for(int j=0;j<temp1.size();j++){
-                    if(temp1.get(j).charAt(i)==userInput.charAt(i)){
-                        temp.add(temp1.get(j));
-                    }
-                }
-            }
-            if(temp.size()<5){
-                temp.addAll(temp1);
-            }
+        System.out.println("\n\nTotal Word: "+ total_wrod+"\n\n\nSuggested word: \n");
 
 
-        System.out.println("Total Word: "+ l2.size()+"\n\n\nSuggested word: \n\n");
-            for(int i=0;i<10;i++){
-                System.out.println(temp1.get(i)+"\n");
-            }
+        list.forEach(System.out::println);
+
         long end = System.currentTimeMillis();
-        System.out.println("Time: "+(end-start));
+        System.out.println("\n\nTime: "+((Double.parseDouble(""+(end-start)))/1000)+" s");
 
     }
 }
